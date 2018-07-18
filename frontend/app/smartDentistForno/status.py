1e72c8e3ef1b255e405833937b71c4dbdcd5c39b
Francesco Grandinetti <francesco.grandinett@gmail.com>
2018-07-18 12:47:33 +0200

    add a class for the device status management

---
+++ frontend/app/smartDentistForno/status.py
@@ -0,0 +1,32 @@
+from sender import MsgSender
+from logger import FileLogger
+
+class StatusHandler:
+    status = False
+    statusFile = None
+    sender = None
+    id = ""
+
+    def __init__(self, msgSender, id):
+        self.statusFile = FileLogger("app/smartDentistForno/doc/statusFile.txt", "wt+")
+        self.sender = msgSender
+        self.id = id
+
+
+    def saveStatus(self,newStatus):
+        if self.changeStatus({'id' : self.id, 'status' : str(newStatus)}):
+            self.sender.sendStatusChangeApp(self.id, self.status)
+
+    #function for changing state when detecting a new command from server
+    def changeStatus(self,data):
+        newStatus = data["status"].lower() == "true"
+        if(data["id"] == self.id and self.status != newStatus):
+            self.status = newStatus
+            statusMsg = "ON" if self.status else "OFF"
+            self.statusFile.log("Switch {}".format(statusMsg))
+            print("Device {} - Switch {}".format(self.id, statusMsg))
+            return True
+        return False
+
+    def isOn(self):
+        return self.status
