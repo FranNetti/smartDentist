b29d4e9984409a1eb4f7bfad7b46add521abbe5e
Francesco Grandinetti <francesco.grandinett@gmail.com>
2018-07-19 16:42:16 +0200

    add cors management to the server

--- backend/app/smartDentist/settings.py
+++ backend/app/smartDentist/settings.py
@@ -22,10 +22,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'smartDentistEP.apps.EndPointConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +124,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = (
+    'GET',
+    'POST',
+)
